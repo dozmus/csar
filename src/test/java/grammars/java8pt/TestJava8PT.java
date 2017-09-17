4ad5b7dec3f890d14aea5de0388dbf29df47d97f
PureCS <purecs@hotmail.com>
2017-09-17 13:26:13 +0100

    Fix TestJava8PT + Java8PT grammar

--- src/test/java/grammars/java8pt/TestJava8PT.java
+++ src/test/java/grammars/java8pt/TestJava8PT.java
@@ -2,9 +2,10 @@ package grammars.java8pt;
 
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.qmul.csar.util.DummyANTLRErrorListener;
 
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
@@ -12,19 +13,27 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static org.junit.Assert.fail;
+
 public final class TestJava8PT {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TestJava8PT.class);
     private static final Path SAMPLES_DIRECTORY = Paths.get("src/test/resources/grammars/java8pt");
 
     @Test
     public void test() throws IOException {
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(SAMPLES_DIRECTORY)) {
             for (Path path : stream) {
-                LOGGER.info("Testing: {}", path.getFileName().toString());
                 JavaLexer lexer = new JavaLexer(CharStreams.fromFileName(path.toString()));
                 CommonTokenStream tokens = new CommonTokenStream(lexer);
                 JavaParser parser = new JavaParser(tokens);
+                parser.addErrorListener(new DummyANTLRErrorListener() {
+                    @Override
+                    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line,
+                            int charPositionInLine, String msg, RecognitionException e) {
+                        fail("RecognitionException thrown in file '" + path.getFileName().toString() + "' at "
+                                + line + ":" + charPositionInLine);
+                    }
+                });
                 parser.compilationUnit();
             }
         }
