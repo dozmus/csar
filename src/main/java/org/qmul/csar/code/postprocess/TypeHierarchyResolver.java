b0ac1695718c5ca90ef8d00bcff5c9feeb0ac281
PureCS <purecs@hotmail.com>
2017-10-15 12:41:06 +0100

    Clean up and document TypeHierarchyResolver

--- src/main/java/org/qmul/csar/code/postprocess/TypeHierarchyResolver.java
+++ src/main/java/org/qmul/csar/code/postprocess/TypeHierarchyResolver.java
@@ -13,11 +13,29 @@ import org.qmul.csar.lang.descriptor.EnumDescriptor;
 import java.nio.file.Path;
 import java.util.*;
 
+/**
+ * A type hierarchy resolver for a {@link Map} of {@link Path} to {@link Statement}.
+ */
 public class TypeHierarchyResolver {
 
+    // TODO parse java api classes properly
     // TODO how do no package stuff work - i.e. do they??
-    private final TypeNode root = new TypeNode("java.lang.Object");
 
+    /**
+     * The node which all others are a child of, in java this is 'java.lang.Object'.
+     */
+    private final TypeNode root = new TypeNode("java.lang.Object");
+    /**
+     * The target type searcher to use.
+     */
+    private final TargetTypeSearcher searcher = new TargetTypeSearcher();
+
+    /**
+     * Resolves the type hierarchy of the argument, and stores it in {@link #root}. If a type hierarchy cannot be
+     * fully resolved it will be added as a partial hierarchy anyway (a non-extensive hierarchy).
+     *
+     * @param code the code to resolve for
+     */
     public void resolve(Map<Path, Statement> code) {
         List<TypeNode> tmp = new ArrayList<>();
 
@@ -43,70 +61,43 @@ public class TypeHierarchyResolver {
         mergePartialTrees(root, tmp);
     }
 
-    private static void mergePartialTrees(TypeNode root, List<TypeNode> tmp) {
-        // TODO ensure no duplicates added?
-        for (TypeNode node : tmp) {
-            if (!insert(root, node)) { // try to insert into correct position, if not possible place it at the root for a partial hierarchy
+    /**
+     * Places all nodes in the argument list into their correct position in the argument {@link TypeNode}.
+     * If a correct position is not found for an element in the list, then they are placed as a child of the
+     * <tt>root</tt> element to form a partial hierarchy.
+     * <p>
+     * If the argument is already contained, it will be added again.
+     *
+     * @param root the node to merge the list into
+     * @param partialHierarchies the list whose elements to merge
+     * @see TypeNode#insert(TypeNode, TypeNode)
+     */
+    private static void mergePartialTrees(TypeNode root, List<TypeNode> partialHierarchies) {
+        for (TypeNode node : partialHierarchies) {
+            if (!TypeNode.insert(root, node)) {
                 root.children.add(node);
             }
         }
     }
 
-    private static boolean insert(TypeNode root, TypeNode node) {
-        if (root.qualifiedName.equals(node.qualifiedName)) {
-            root.children.addAll(node.children);
-            return true;
-        } else {
-            for (TypeNode rootChildren : root.children) {
-                boolean inserted = insert(rootChildren, node);
-
-                if (inserted)
-                    return true;
-            }
-        }
-        return false;
-    }
-
-    private TypeNode getFromTmp(List<TypeNode> tmp, String qualifiedName) {
-        for (int i = 0; i < tmp.size(); i++) {
-            TypeNode node = tmp.get(i);
-
-            if (node.containsQualifiedName(qualifiedName)) {
-                tmp.remove(i);
-                return node;
-            }
-        }
-        return new TypeNode(qualifiedName);
-    }
-
     private String resolveQualifiedName(Map<Path, Statement> code, Optional<PackageStatement> currentPackage,
             List<ImportStatement> imports, String name) {
-        if (name.contains(".")) { // a an inner name, i.e. MyClass.InnerType or a fully qualified name
-            // Check classes in same package
-            String s = resolvedInCurrentPackage(code, currentPackage, name);
+        // Resolve against classes in same package
+        String s = resolvedInCurrentPackage(code, currentPackage, name);
 
-            if (s != null)
-                return s;
+        if (s != null)
+            return s;
 
-            // Check imports
-            String s2 = resolveInOtherPackages(code, imports, name);
+        // Resolve against imports
+        String s2 = resolveInOtherPackages(code, imports, name);
 
-            if (s2 != null)
-                return s2;
-            return name; // if no match, we assume its an external class
-        } else { // need to resolve a simple name, i.e. MyClass
-            // Check classes in same package
-            String s = resolvedInCurrentPackage(code, currentPackage, name);
+        if (s2 != null)
+            return s2;
 
-            if (s != null)
-                return s;
-
-            // Check imports
-            String s2 = resolveInOtherPackages(code, imports, name);
-
-            if (s2 != null)
-                return s2;
-        }
+        // If name contains dots, we assume it is a fully qualified name
+        // TODO check this properly
+        if (name.contains("."))
+            return name;
         throw new RuntimeException("could not resolve qualified name for  " + name);
     }
 
@@ -165,80 +156,110 @@ public class TypeHierarchyResolver {
         return null;
     }
 
-    private boolean targetContainsName(String currentPkg, String otherPkg, TypeStatement target, String name) {
+    /**
+     * Returns <tt>true</tt> if the target type statement contains the given qualified name. If <tt>currentPkg</tt>
+     * and <tt>otherPkg</tt> are not equal, then <tt>false</tt> is returned.
+     *
+     * @param currentPkg the package <tt>qualifiedName</tt> is defined in
+     * @param otherPkg the package target is in
+     * @param target the target type statement
+     * @param qualifiedName the qualified name to check is contained in the target
+     * @return returns <tt>true</tt> if the target type statement contains the given qualified name
+     * @see TargetTypeSearcher
+     * @see #searcher
+     */
+    private boolean targetContainsName(String currentPkg, String otherPkg, TypeStatement target, String qualifiedName) {
         // Compare packages
         if (!otherPkg.equals(currentPkg))
             return false;
 
         // Perform search in target
-        TargetTypeSearcher searcher = new TargetTypeSearcher(name);
+        searcher.resetState(qualifiedName);
         searcher.visit(target);
         return searcher.isMatched();
     }
 
-    private void placeInList(List<TypeNode> tmp, Map<Path, Statement> code, Optional<PackageStatement> packageStatement,
-            List<ImportStatement> imports, String child, List<String> superClasses) {
+    private void placeInList(List<TypeNode> list, Map<Path, Statement> code,
+            Optional<PackageStatement> packageStatement, List<ImportStatement> imports, String child,
+            List<String> superClasses) {
         for (String superClass : superClasses) {
             String resolvedSuperClassName = resolveQualifiedName(code, packageStatement, imports, superClass);
 
             // add to tmp structure, if you cant place it then add it as a new child
-            if (!placeInList(tmp, resolvedSuperClassName, child)) {
+            if (!placeInList(list, resolvedSuperClassName, child)) {
                 TypeNode node = new TypeNode(resolvedSuperClassName);
                 node.children.add(new TypeNode(child));
-                tmp.add(node);
+                list.add(node);
             }
         }
     }
 
-    private static boolean placeInList(List<TypeNode> tmp, String parent, String child) {
-        for (TypeNode node : tmp) {
+    /**
+     * Returns <tt>true</tt> if the argument <tt>child</tt> was added to the argument <tt>parent</tt>s children as a
+     * new {@link TypeNode}.
+     *
+     * @param list the list to search for the parent for
+     * @param parent the qualified name of the parent
+     * @param child the qualified name of the child
+     * @return <tt>true</tt> if the argument <tt>child</tt> was added to the argument <tt>parent</tt>
+     */
+    private static boolean placeInList(List<TypeNode> list, String parent, String child) {
+        for (TypeNode node : list) {
             if (node.qualifiedName.equals(parent)) {
                 node.children.add(new TypeNode(child));
                 return true;
             }
-            boolean addedToChildren = placeInList(node.children, parent, child);
 
-            if (addedToChildren)
+            if (placeInList(node.children, parent, child))
                 return true;
         }
         return false;
     }
 
     /**
-     * Returns if the first type is a superclass of the second type, this means it returns <tt>true</tt> if they are
-     * equal.
+     * Returns <tt>true</tt> if the first type is a superclass of, or equal to, the second type.
      *
-     * @param type1 a fully qualified type which may be a superclass
-     * @param type2 a fully qualified type which may be a subclass
+     * @param type1 a qualified name which may be a superclass
+     * @param type2 a qualified name which may be a subclass
      * @return returns if the first type is a superclass of the second type
      */
     public boolean isSubtype(String type1, String type2) {
-        return type1.equals(type2) || isSubtype(root, type1, type2);
+        return type1.equals(type2) || isStrictlySubtype(root, type1, type2);
     }
 
-    private static boolean isSubtype(TypeNode root, String type1, String type2) {
-        // Base node
+    /**
+     * Returns <tt>true</tt> if the first type is a superclass of the second type.
+     *
+     * @param root the node hierarchy to check
+     * @param type1 a qualified name which may be a superclass
+     * @param type2 a qualified name which may be a subclass
+     * @return returns if the first type is a superclass of the second type
+     */
+    private static boolean isStrictlySubtype(TypeNode root, String type1, String type2) {
         if (root.qualifiedName.equals(type1)) {
             return root.containsQualifiedName(type2);
         } else {
             for (TypeNode child : root.children) {
-                if (isSubtype(child, type1, type2))
+                if (isStrictlySubtype(child, type1, type2))
                     return true;
             }
         }
         return false;
     }
 
+    /**
+     * A node representing the type hierarchy of a single type.
+     */
     private static final class TypeNode {
 
-        private String qualifiedName;
+        private final String qualifiedName;
         private final List<TypeNode> children = new ArrayList<>();
 
-        public TypeNode(String qualifiedName) {
+        TypeNode(String qualifiedName) {
             this.qualifiedName = qualifiedName;
         }
 
-        public boolean containsQualifiedName(String qualifiedName) {
+        boolean containsQualifiedName(String qualifiedName) {
             return containsQualifiedName(this, qualifiedName, false);
         }
 
@@ -250,8 +271,18 @@ public class TypeHierarchyResolver {
                     .toString();
         }
 
+        /**
+         * Returns <tt>true</tt> if the argument <tt>String</tt> is contained in any child of the argument
+         * <tt>TypeNode</tt>. The argument <tt>TypeNode</tt>'s value is only checked if <tt>checkCurrent</tt> is
+         * <tt>true</tt>.
+         *
+         * @param node the node to check
+         * @param qualifiedName the name to check for
+         * @param checkCurrent if the current node's value should be checked too
+         * @return if the argument node contains the argument qualifiedName
+         */
         private static boolean containsQualifiedName(TypeNode node, String qualifiedName, boolean checkCurrent) {
-            if (node.qualifiedName.equals(qualifiedName) && checkCurrent) {
+            if (checkCurrent && node.qualifiedName.equals(qualifiedName)) {
                 return true;
             } else {
                 for (TypeNode child : node.children) {
@@ -261,17 +292,44 @@ public class TypeHierarchyResolver {
                 return false;
             }
         }
+
+        /**
+         * Returns <tt>true</tt> if the attempted insertion was successful, it can only be made if a node in
+         * <tt>root</tt> is found which has the same <tt>qualifiedName</tt> as <tt>node</tt>, then all of the children
+         * of <tt>node</tt> are added to <tt>root</tt>. This returns after a single insertion at most.
+         * <p>
+         * If the argument is already contained, it will be added again.
+         *
+         * @param root the node to add to
+         * @param node the node whose children to add
+         * @return <tt>true</tt> if insertion was successful
+         */
+        private static boolean insert(TypeNode root, TypeNode node) {
+            if (root.qualifiedName.equals(node.qualifiedName)) {
+                root.children.addAll(node.children);
+                return true;
+            } else {
+                for (TypeNode child : root.children) {
+                    if (insert(child, node))
+                        return true;
+                }
+            }
+            return false;
+        }
     }
 
     private static final class TargetTypeSearcher extends StatementVisitor {
 
-        private final String[] targetName;
+        private String[] targetName;
         private int matches = 0;
         private int nesting = -1;
         private boolean cancelled;
 
-        public TargetTypeSearcher(String targetName) {
+        public void resetState(String targetName) {
             this.targetName = targetName.split("\\.");
+            matches = 0;
+            nesting = -1;
+            cancelled = false;
         }
 
         public void visit(Statement statement) {
@@ -282,21 +340,21 @@ public class TypeHierarchyResolver {
 
         @Override
         public void visitClassStatement(ClassStatement statement) {
-            preVisit(statement.getDescriptor().getIdentifierName());
+            nesting++;
+            attemptMatch(statement.getDescriptor().getIdentifierName());
             super.visitClassStatement(statement);
             nesting--;
         }
 
         @Override
         public void visitEnumStatement(EnumStatement statement) {
-            preVisit(statement.getDescriptor().getIdentifierName());
+            nesting++;
+            attemptMatch(statement.getDescriptor().getIdentifierName());
             super.visitEnumStatement(statement);
             nesting--;
         }
 
-        private void preVisit(IdentifierName identifierName) {
-            nesting++;
-
+        private void attemptMatch(IdentifierName identifierName) {
             if (nesting >= targetName.length) {
                 cancelled = true;
             } else if (!isMatched() && identifierName.toString().equals(targetName[nesting])) {
@@ -304,7 +362,7 @@ public class TypeHierarchyResolver {
             }
         }
 
-        public boolean isMatched() {
+        boolean isMatched() {
             return matches == targetName.length;
         }
     }
@@ -315,27 +373,17 @@ public class TypeHierarchyResolver {
         private final List<TypeNode> tmp;
         private final List<ImportStatement> imports;
         private final Optional<PackageStatement> packageStatement;
+        private final Map<Integer, String> map = new HashMap<>(); // maps 'nesting number' to 'prefix' at that nesting
         private String currentIdentifierName;
         private int nesting = 0;
-        private Map<Integer, String> map = new HashMap<>(); // 'nesting number' mapped to the 'prefix' (at that nesting)
 
-        public TypeResolver(Map<Path, Statement> code, List<TypeNode> tmp, String currentPkg,
+        TypeResolver(Map<Path, Statement> code, List<TypeNode> tmp, String currentPkg,
                 List<ImportStatement> imports, Optional<PackageStatement> packageStatement) {
             this.code = code;
             this.tmp = tmp;
             this.imports = imports;
             this.packageStatement = packageStatement;
             this.currentIdentifierName = currentPkg;
-        }
-
-        @Override
-        public void visitMethodStatement(MethodStatement statement) {
-            super.visitMethodStatement(statement);
-        }
-
-        @Override
-        public void visitBlockStatement(BlockStatement statement) {
-            super.visitBlockStatement(statement);
         }
 
         @Override
@@ -363,11 +411,31 @@ public class TypeHierarchyResolver {
             currentIdentifierName = map.get (nesting) + (nesting > 0 ? "$" : "") + identifierName;
 
             if (superClasses.size() == 0) {
-                root.children.add(getFromTmp(tmp, currentIdentifierName));
+                root.children.add(getFromListOrDefault(tmp, currentIdentifierName));
             } else {
                 placeInList(tmp, code, packageStatement, imports, currentIdentifierName, superClasses);
             }
             nesting++;
+        }
+
+        /**
+         * Returns a node with the given <tt>qualifiedName</tt> after removing it from the argument list, or creates and
+         * returns a new {@link TypeNode} with that qualified name.
+         *
+         * @param tmp the list to search
+         * @param qualifiedName the qualified name to search for
+         * @return a <tt>TypeNode</tt> with the given qualified name
+         */
+        private TypeNode getFromListOrDefault(List<TypeNode> tmp, String qualifiedName) {
+            for (int i = 0; i < tmp.size(); i++) {
+                TypeNode node = tmp.get(i);
+
+                if (node.containsQualifiedName(qualifiedName)) {
+                    tmp.remove(i);
+                    return node;
+                }
+            }
+            return new TypeNode(qualifiedName);
         }
     }
 }
